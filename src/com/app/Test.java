8e95ae9c484f123475f33de3ae3788169c868f69
raj kumar <raj kumar@HP>
2019-03-15 11:52:35 +0530

    deleted by raj

--- src/com/app/Test.java
+++ src/com/app/Test.java
@@ -5,14 +5,11 @@ public class Test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.println("this is mr.ramkumar");
 
-		System.out.println("made changes here by raj");
 		
 		System.out.println("made changes here by rajkumr2201");
 		
 		System.out.println("made changes here by rajkumr reddy");
-		System.out.println("made changes here by rajkumr reddy2");
 		
 	}
 
